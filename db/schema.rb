2cf9ed39e8b60d6dd52cafc38022465ed20f5251
kinoppyd <ppyd@kinoppyd.dev>
2024-11-30 22:37:59 +0900

    bin/rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_11_30_132459) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_30_133619) do
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "sessions", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "ip_address"
@@ -35,5 +43,6 @@ ActiveRecord::Schema[8.0].define(version: 2024_11_30_132459) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "posts", "users"
   add_foreign_key "sessions", "users"
 end
